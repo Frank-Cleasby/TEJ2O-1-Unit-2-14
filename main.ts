ae32d3dd15f0d170ff8fcdbc88df1c93c0fdbdbb
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-10-30 12:12:24 -0400

    done

--- main.ts
+++ main.ts
@@ -1,8 +1,89 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Frank
+ * Created on: Oct 2025
+ * This program moves a pixel in clock ways
 */
 
-basic.showString('Hello, World!')
+// variables
+let sprite: game.LedSprite = null
+let xVariable: number = 0
+let yVariable: number = 0
+
+// clean
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// pixel move clockwise
+input.onButtonPressed(Button.A, function () {
+    xVariable = 0
+    yVariable = 0
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+    sprite.set(LedSpriteProperty.X, xVariable)
+    sprite.set(LedSpriteProperty.Y, yVariable)
+
+    while (xVariable < 5) {
+        sprite.set(LedSpriteProperty.X, xVariable)
+        xVariable = xVariable + 1
+        basic.pause(200)
+    }
+
+    while (yVariable < 5) {
+        sprite.set(LedSpriteProperty.Y, yVariable)
+        yVariable = yVariable + 1
+        basic.pause(200)
+    }
+
+    while (xVariable >= 0) {
+        sprite.set(LedSpriteProperty.X, xVariable)
+        xVariable = xVariable - 1
+        basic.pause(200)
+    }
+
+    while (yVariable >= 0) {
+        sprite.set(LedSpriteProperty.Y, yVariable)
+        yVariable = yVariable - 1
+        basic.pause(200)
+    }
+    basic.pause(500)
+    sprite.delete()
+    basic.showIcon(IconNames.Giraffe)
+})
+
+// pixel move counter clockwise
+input.onButtonPressed(Button.B, function () {
+    xVariable = 5
+    yVariable = 5
+    basic.clearScreen()
+    sprite = game.createSprite(5, 5)
+    sprite.set(LedSpriteProperty.X, xVariable)
+    sprite.set(LedSpriteProperty.Y, yVariable)
+
+    while (xVariable >= 0) {
+        sprite.set(LedSpriteProperty.X, xVariable)
+        xVariable = xVariable - 1
+        basic.pause(200)
+    }
+
+    while (yVariable >= 0) {
+        sprite.set(LedSpriteProperty.Y, yVariable)
+        yVariable = yVariable - 1
+        basic.pause(200)
+    }
+
+    while (xVariable < 5) {
+        sprite.set(LedSpriteProperty.X, xVariable)
+        xVariable = xVariable + 1
+        basic.pause(200)
+    }
+
+    while (yVariable < 5) {
+        sprite.set(LedSpriteProperty.Y, yVariable)
+        yVariable = yVariable + 1
+        basic.pause(200)
+    }
+    basic.pause(500)
+    sprite.delete()
+    basic.showIcon(IconNames.Giraffe)
+})
